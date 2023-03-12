3a8b1a7485ccb8f222609f0883f55e426aa7b2c2
Mougily <92stephaniejones@gmail.com>
2023-03-12 15:20:09 +0000

    initial commit

--- example.js
+++ example.js
@@ -1,29 +1,17 @@
 // Type Annotations
-
-let myName: string = "SJ";
-let isCool: boolean = true;
-let favNumber: number = 30;
-
-function add(a: number, b: number) {
-  return a + b;
+var myName = "SJ";
+var isCool = true;
+var favNumber = 30;
+function add(a, b) {
+    return a + b;
 }
-
 console.log(add(4, 2));
-
 // Inferred Types
-
 // Union Types
-
 // Objects and Interfaces
-
 // Optional Properties
-
 // Classes
-
 // Generics
-
 // any, unknown, never
-
 // Type Assertions
-
 // @ts-ignore / @ts-nocheck
